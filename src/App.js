3cce161b6625f9e814091a9ff1080e47ca614595
daniellehemet <dmhemet@hotmail.ca>
2023-02-02 19:38:05 -0500

    got firebase data rendering onto page, added add to cart button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+// Modules
+import firebase from './firebase';
+import {useEffect, useState} from 'react';
+import {getDatabase, ref, onValue} from 'firebase/database';
+// Components
+
+// Assets
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const database = getDatabase(firebase)
+    const dbRef = ref(database)
+    onValue(dbRef, (response) => {
+      const newState = [];
+      const data = response.val();
+      for (let key in data) {
+        newState.push(data[key]);
+      }
+      setProducts(newState);
+    })
+  }, [])
+
+  const handleAddToCart = () => {
+    setCart()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MagicShop</h1>
+      <ul>
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              <p>{product.name}</p>
+              <p>{product.price} GP</p>
+              <button onClick={handleAddToCart}>Add To Cart</button>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
