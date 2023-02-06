e92acb0186d09f3a4813b8e52124e924c37930d7
daniellehemet <dmhemet@hotmail.ca>
2023-02-06 17:55:17 -0500

    handleAddToCart function and cart counter revised, additional styling

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import Header from "./Components/Header";
 import './App.css';
 
 function App() {
+  // products state for inventory
   const [products, setProducts] = useState([]);
+  // cart state for cart items
   const [cart, setCart] = useState([]);
-  const [counter, setCounter] = useState(0);
+
 
   useEffect(() => {
     const database = getDatabase(firebase)
@@ -25,20 +27,23 @@ function App() {
     })
   }, [])
 
-  const handleAddToCart = () => {
-    setCart()
+
+  const handleAddToCart = (product) => {
+    const copy = [...cart]
+    copy.push(product)
+    setCart(copy)
   }
 
   return (
     <div className="App">
-      <Header />
+      <Header itemsInCart={cart.length}/>
       <ul>
         {products.map((product) => {
           return (
             <li key={product.id}>
               <p>{product.name}</p>
               <p>{product.price} GP</p>
-              <button onClick={handleAddToCart}>Add To Cart</button>
+              <button onClick={() => { handleAddToCart(product) }}>Add To Cart</button>
             </li>
           )
         })}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,10 @@
-const Header = () => {
+const Header = (props) => {
   return (
     <div className="header">
+      <h2>Welcome adventurers...</h2>
       <h1>MagicShop</h1>
-      <p>Welcome adventurers...</p>
+      {/* want counter value to update here */}
+      <p>{props.itemsInCart} items in cart</p>
     </div>
   )
 }
