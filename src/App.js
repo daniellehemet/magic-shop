314c10e94c820a4f4e60ce3089cc537cebc0ad5c
daniellehemet <dmhemet@hotmail.ca>
2023-02-07 17:33:28 -0500

    restructured cart data for remove functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // Modules
 import firebase from './firebase';
 import {useEffect, useState} from 'react';
-import {getDatabase, ref, onValue} from 'firebase/database';
+import {getDatabase, ref, onValue, push} from 'firebase/database';
 // Components
 import Header from "./Components/Header";
 import Cart from "./Components/Cart";
@@ -17,7 +17,9 @@ function App() {
 
   useEffect(() => {
     const database = getDatabase(firebase)
-    const dbRef = ref(database)
+    const dbRef = ref(database, "/inventory")
+    const cartRef = ref(database, "/cart")
+    // pulling inventory data
     onValue(dbRef, (response) => {
       const newState = [];
       const data = response.val();
@@ -26,31 +28,42 @@ function App() {
       }
       setProducts(newState);
     })
+    // pulling cart data
+    onValue(cartRef, (response) =>{
+      const newState = [];
+      const cartData = response.val();
+      for (let key in cartData) {
+        newState.push({
+          fbID: key, 
+          data: cartData[key]
+      });
+      }
+      setCart(newState);
+    })
   }, [])
 
-
   const handleAddToCart = (product) => {
-    const copy = [...cart]
-    copy.push(product)
-    setCart(copy)
+    const database = getDatabase(firebase)
+    const cartRef = ref(database, "/cart")
+    push(cartRef, product)
   }
 
   return (
     <div className="App">
       <Header itemsInCart={cart.length}/>
       <div className='wrapper'>
-        <ul>
+        <ul className='inventory'>
           {products.map((product) => {
             return (
-              <li key={product.id}>
+              <li className="inventory" key={product.id}>
                 <p className='product-name'>{product.name}</p>
                 <p className='price'>Price: {product.price} GP</p>
-                <button onClick={() => { handleAddToCart(product) }}>Add To Cart</button>
+                <button className="add-to-cart" onClick={() => { handleAddToCart(product) }}>Add To Cart</button>
               </li>
             )
           })}
         </ul>
-        <Cart />
+        <Cart cart={cart}/>
       </div>
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,6 @@ const Header = (props) => {
     <div className="header">
       <h2>Welcome adventurers...</h2>
       <h1>MagicShop</h1>
-      {/* want counter value to update here */}
       <p>{props.itemsInCart} items in cart</p>
     </div>
   )
